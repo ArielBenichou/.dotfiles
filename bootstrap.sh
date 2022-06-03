1ed8e060b19cd79d48afd8d4dedb2994280a9b34
Ariel Benichou <videojr3000@users.noreply.github.com>
2022-06-03 15:22:09 +0300

    vim

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
+mkdir -p ./.vim ./.vim/autoload ./.vim/backup ./.vim/colors ./.vim/plugged
+
+curl -fLo ./.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
+
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.vim ~/.vim
